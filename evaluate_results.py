4acfab276867770d67a748ee53cfb7950c0dc686
mpentek <mpentek@gmail.com>
2018-07-09 11:03:49 +0200

    Reorganizing system arguments to be passed

--- evaluate_results.py
+++ evaluate_results.py
@@ -15,7 +15,7 @@ import json
 import sys
 from os import path as os_path
 
-import re
+from argparse import ArgumentParser
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
 
@@ -25,36 +25,46 @@ from utilities.other_utilities import get_ramp_up_index, get_cp_series
 from utilities.statistic_utilities import get_general_statistics, get_extreme_values_statistics
 from utilities.plot_utilities import plot_ref_point_pressure_results, plot_pressure_tap_cp_results, plot_pressure_taps_general_statistics, plot_pressure_taps_extreme_values
 
-try:
-    # test if system argument is passed for testing
-    is_test = bool(sys.argv[1])
-except:
-    # use default testing setting
-    # all files will be evaluated
-    is_test = False
+#----------------------------------------------------------------
+# system arguments which can be passed and default values
+parser = ArgumentParser()
 
-if is_test:
+# add more options if you like
+parser.add_argument("--nr_of_blocks", dest="nr_of_blocks", type=int, default=6,
+                    help="int: number of blocks for Block-axima")
+parser.add_argument("--calculate_mode", dest="calculate_mode", type=bool, default=False,
+                    help="bool: calculate_mode when calling statistics, can slow down computation if True")
+parser.add_argument("--is_test", dest="is_test", type=bool, default=True,
+                    help="bool: is_test to evaluate on very few files for testing, can speed up computation if True")
+
+args = parser.parse_args()
+
+if args.calculate_mode:
+    print("## Mode calculation on, might take longer")
+
+if args.is_test:
     results_overview = 'ResultsOverviewTest.json'
     report_case_ending = 'Test.pdf'
+    print("## In testing mode")
 else:
     results_overview = 'ResultsOverview.json'
     report_case_ending = '.pdf'
+    print("## In all evaluation mode")
 
-# variables which need to be additionally specified
-# for the block maxima
-nr_of_blocks = 6
-# in csae needed, but warning because it increases evaluation time reasonably
-calculate_mode = True
-if calculate_mode:
-    print("## Mode calculation on, might take longer")
+#----------------------------------------------------------------
+# hardcoded parameters
+
 # subfolders where the input data is and where reports should be generated
 input_data_folder = 'input_data'
 reports_folder = 'reports'
 
+#----------------------------------------------------------------
 # load results parameters
 with open(os_path.join(input_data_folder, results_overview)) as f:
     results = json.load(f)['results']
 
+#----------------------------------------------------------------
+# evaluate results
 for result in results:
     with PdfPages(os_path.join(reports_folder, 'LowriseReport_' + result['case'] + report_case_ending)) as report_pdf:
 
@@ -69,7 +79,7 @@ for result in results:
         # evaluating statistical quantities
         result['reference_points'][0]['statistics'] = {}
         result['reference_points'][0]['statistics']['pressure'] = {}
-        result['reference_points'][0]['statistics']['pressure']['general'] = get_general_statistics(result['reference_points'][0]['series']['pressure'], calculate_mode)
+        result['reference_points'][0]['statistics']['pressure']['general'] = get_general_statistics(result['reference_points'][0]['series']['pressure'], args.calculate_mode)
 
         # plotting reference point data
         plot_ref_point_pressure_results(result['reference_points'][0], report_pdf)
@@ -94,14 +104,14 @@ for result in results:
             pressure_tap['statistics'] = {}
             pressure_tap['statistics']['cp'] = {}
             pressure_tap['statistics']['cp']['general'] = get_general_statistics(pressure_tap['series']['cp'][result['reference_points'][0]['post_ramp_up_index']:],
-                                                calculate_mode)
+                                                args.calculate_mode)
             pressure_tap['statistics']['cp']['extreme_value'] = get_extreme_values_statistics(pressure_tap['series']['cp'][pressure_tap['post_ramp_up_index']:],
                                                 pressure_tap['post_ramp_up_index'],
-                                                nr_of_blocks,
-                                                calculate_mode)
+                                                args.nr_of_blocks,
+                                                args.calculate_mode)
 
             # plotting tap data
-            plot_pressure_tap_cp_results(pressure_tap, calculate_mode, report_pdf)
+            plot_pressure_tap_cp_results(pressure_tap, args.calculate_mode, report_pdf)
 
             print('## Plot for result case ' + result['case'] + ' and tap label ' + pressure_tap['label'] + ' ready')
 
@@ -109,7 +119,7 @@ for result in results:
         plot_pressure_taps_general_statistics(result['pressure_taps'], report_pdf)
 
         # extreme value statistics for all taps
-        plot_pressure_taps_extreme_values(result['pressure_taps'], calculate_mode, report_pdf)
+        plot_pressure_taps_extreme_values(result['pressure_taps'], args.calculate_mode, report_pdf)
 
         print('## All plots for result case ' + result['case'] + ' finished')
         # "clearing" dictionary value to reduce memory consumption
